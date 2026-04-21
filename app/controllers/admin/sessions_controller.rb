a0224c1d6ef4b89f092c559dde47b284adff1c7a
nakano-shotaro <sononakano@gmail.com>
2026-04-21 22:14:56 +0900

    feat: その他のユーザー認証(2)を追加

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,37 @@
+class Admin::SessionsController < Admin::Base 
+  def new 
+    if current_administrator 
+      redirect_to :admin_root   
+    else 
+      @form = Admin::LoginForm.new 
+      render action: "new", status: :unprocessable_entity   
+    end         
+  end 
+  
+  def create 
+    @form = Admin::LoginForm.new(params[:admin_login_form]) 
+    if @form.email.present? 
+      administrator = 
+        Administrator.find_by("LOWER(email) = ?", @form.email.downcase)   
+    end 
+    if Admin::Authenticator.new(administrator).authenticate(@form.password) 
+      if administrator.suspended? 
+        flash.now.alert = "アカウントが停止されています。"
+        render action: "new", status: :unprocessable_entity 
+      else   
+        session[:administrator_id] = administrator.id 
+        flash.notice = "ログインしました。"
+        redirect_to :admin_root 
+      end     
+    else 
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
+      render action: "new", status: :unprocessable_entity     
+    end        
+  end 
+  
+  def destroy 
+    session.delete(:administrator_id) 
+    flash.notice = "ログアウトしました。"
+    redirect_to :admin_root, status: :see_other  
+  end   
+end
