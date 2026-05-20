60bee80ade4a55e89584a416621f3d9bd881fcc3
nakano-shotaro <sononakano@gmail.com>
2026-05-20 11:08:05 +0900

    feat: その他のページネーションを追加

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -8,7 +8,8 @@ class Admin::StaffMembersController < Admin::Base
     #unless current_administrator
       #redirect_to :admin_login
     #end
-    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)  
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+      .page(params[:page])   
   end 
 
   def show 
