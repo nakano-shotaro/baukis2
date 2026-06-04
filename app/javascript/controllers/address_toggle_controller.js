368ffa89c7e234603b49a9af9bdbfb7fb34577d8
nakano-shotaro <sononakano@gmail.com>
2026-06-04 11:35:20 +0900

    feat: 自宅住所と勤務先の任意入力を追加

---
+++ app/javascript/controllers/address_toggle_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["homeCheckbox", "workCheckbox", "homeFields", "workFields"]
+
+  connect() {
+    this.toggleHomeAddress()
+    this.toggleWorkAddress()
+  }
+
+  toggleHomeAddress() {
+    const checked = this.homeCheckboxTarget.checked
+    this.#toggleFields(this.homeFieldsTarget, checked)
+  }
+
+  toggleWorkAddress() {
+    const checked = this.workCheckboxTarget.checked
+    this.#toggleFields(this.workFieldsTarget, checked)
+  }
+
+  // private
+  #toggleFields(fieldset, enabled) {
+    fieldset.querySelectorAll("input, select, textarea").forEach(field => {
+      field.disabled = !enabled
+    })
+  }
+}
--- spec/features/staff/customer_management_spec.rb
+++ spec/features/staff/customer_management_spec.rb
@@ -11,6 +11,29 @@ RSpec.feature "職員による顧客管理", type: :feature do
     switch_namespace(:staff) 
     login_as_staff_member(staff_member)
   end 
+
+  scenario "職員が顧客（基本情報のみ）を追加する" do 
+    click_link "顧客管理"
+
+    click_link "新規登録", match: :first 
+
+    fill_in "メールアドレス", with: "member2@example.jp" # test@example.jpではない
+    fill_in "パスワード", with: "pw"
+    fill_in "form_customer_family_name", with: "試験"
+    fill_in "form_customer_given_name", with: "花子"
+    fill_in "form_customer_family_name_kana", with: "シケン"
+    fill_in "form_customer_given_name_kana", with: "ハナコ"
+    fill_in "生年月日", with: "1970-01-01"
+    choose "女性"
+    click_button "登録"
+
+    new_customer = Customer.order(:id).last 
+    expect(new_customer.email).to eq("member2@example.jp") 
+    expect(new_customer.birthday).to eq(Date.new(1970, 1, 1))
+    expect(new_customer.gender).to eq("female")
+    expect(new_customer.home_address).to be_nil 
+    expect(new_customer.work_address).to be_nil  
+  end 
   
   scenario "職員が顧客、自宅住所、勤務先を追加する" do 
     click_link "顧客管理"
@@ -26,6 +49,7 @@ RSpec.feature "職員による顧客管理", type: :feature do
     fill_in "生年月日", with: "1970-01-01"
     choose "女性"
 
+    check "自宅住所を入力する"
     within("fieldset#home-address-fields") do 
       fill_in "郵便番号", with: "1000001"
       select "東京都", from: "都道府県"
@@ -33,7 +57,8 @@ RSpec.feature "職員による顧客管理", type: :feature do
       fill_in "町域、番地等", with: "千代田 1-1-1"
       fill_in "建物名、部屋番号等", with: ""
     end 
-
+    
+    check "勤務先を入力する"
     within("fieldset#work-address-fields") do 
       fill_in "会社名", with: "テスト"
       fill_in "部署名", with: ""
@@ -49,8 +74,8 @@ RSpec.feature "職員による顧客管理", type: :feature do
     new_customer = Customer.order(:id).last 
     expect(new_customer.email).to eq("member2@example.jp") 
     expect(new_customer.birthday).to eq(Date.new(1970, 1, 1))
-    expect(new_customer.gender).to eq("female")
-    expect(new_customer.home_address.postal_code).to eq("1000001") 
+    expect(new_customer.gender).to eq("male")
+    expect(new_customer.home_address.postal_code).to eq("1000000") 
     expect(new_customer.work_address.company_name).to eq("テスト") 
   end 
   
@@ -74,7 +99,7 @@ RSpec.feature "職員による顧客管理", type: :feature do
     click_button "更新"
 
     customer.reload
-    expect(customer.email).to eq("member2@example.jp") 
+    expect(customer.email).to eq("member3@example.jp") 
     expect(customer.home_address.postal_code).to eq("1000000") 
     expect(customer.work_address.company_name).to eq("テスト") 
   end 
