ae8029970e0c1885e1b87b4efc68b2277b797ce0
nakano-shotaro <sononakano@gmail.com>
2026-06-06 15:04:19 +0900

    feat: 顧客番号の管理(2)を追加

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -2,15 +2,22 @@ class Staff::CustomerForm
   include ActiveModel::Model 
 
   attr_accessor :customer, :inputs_home_address, :inputs_work_address  
-  delegate :persisted?, :save, to: :customer 
+  #delegate :persisted?, :save, to: :customer 
+  delegate :persisted?, to: :customer 
 
   def initialize(customer = nil) 
     @customer = customer
     @customer ||= Customer.new(gender: "male") 
+    (2 - @customer.personal_phones.size).times do 
+      @customer.personal_phones.build 
+    end   
     self.inputs_home_address = @customer.home_address.present? 
     self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address 
-    @customer.build_work_address unless @customer.work_address
+    @customer.build_work_address unless @customer.work_address 
+    (2 - @customer.home_address.phones.size).times do 
+      @customer.home_address.phones.build 
+    end   
   end 
   
   def assign_attributes(params = {}) 
@@ -20,8 +27,53 @@ class Staff::CustomerForm
 
     customer.assign_attributes(customer_params)
 
+    phones = phone_params(:customer).fetch(:phones, {})  
+    
+    # 既存の personal_phones を配列としてローカル変数に退避させる
+    # (動的なソートや破壊フラグによるActiveRecord内部の配列崩れを防ぐため)
+    current_phones = customer.personal_phones.to_a
+    
+    2.times do |index| 
+      attributes = phones[index.to_s] 
+      # 既存のオブジェクトがあればそれを使い、なければ新しく build する
+      phone = current_phones[index] || customer.personal_phones.build
+      
+      # 【重要】親オブジェクトを明示的に紐付ける
+      phone.customer = customer 
+
+      if attributes && attributes[:number].present? 
+        phone.assign_attributes(attributes) 
+      else 
+        # すでに保存済みのデータなら削除マーク、未保存ならコレクションから安全に削除
+        if phone.persisted?
+          phone.mark_for_destruction
+        else
+          customer.personal_phones.delete(phone)
+        end
+      end   
+    end
+
+    #customer.personal_phones.size.times do |index| 
+      #attributes = phones[index.to_s] 
+      #if attributes && attributes[:number].present? 
+        #customer.personal_phones[index].assign_attributes(attributes) 
+      #else 
+        #customer.personal_phones[index].mark_for_destruction  
+      #end   
+    #end   
+
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
+      phones = phone_params(:home_address).fetch(:phones)
+
+      customer.home_address.phones.size.times do |index| 
+        attributes = phones[index.to_s] 
+        if attributes && attributes[:number].present? 
+          customer.home_address.phones[index].assign_attributes(attributes)
+        else 
+          customer.home_address.phones[index].mark_for_destruction 
+        end     
+      end   
     else 
       customer.home_address.mark_for_destruction   
     end 
@@ -30,27 +82,43 @@ class Staff::CustomerForm
       customer.work_address.assign_attributes(work_address_params)
     else 
       customer.work_address.mark_for_destruction    
-    end   
+    end 
+    p phones   
   end 
 
-  #def valid? 
-    #[ customer, customer.home_address, customer.work_address ]
-      #.map(&:valid?).all?  
-  #end   
+  def valid? 
+    # 顧客、自宅住所、勤務先住所、および電話番号（削除マーク付き以外）のすべてを検証
+    valid_phones = customer.personal_phones.reject(&:marked_for_destruction?).map(&:valid?).all?
 
-  #def save 
-    ##if valid?
-      ##ActiveRecord::Base.transaction do 
-        ##customer.save!
-        ##customer.home_address.save!
-        ##customer.work_address.save!
-      ##end 
-    ##end 
+    [ customer, customer.home_address, customer.work_address ]
+      .map(&:valid?).all? && valid_phones 
+  end   
+
+  def save 
+    if valid?
+      ActiveRecord::Base.transaction do 
+        # 削除マークがついている電話番号を物理削除、それ以外を保存
+        customer.personal_phones.each do |phone|
+          if phone.marked_for_destruction?
+            phone.destroy!
+          else
+            phone.save!
+          end
+        end
+
+        customer.save!
+        customer.home_address.save!
+        customer.work_address.save!
+      end 
+      true 
+    else 
+      false   
+    end    
     #customer.save  
-  #end   
+  end   
 
   private def customer_params 
-    @params.require(:customer).permit(
+    @params.require(:customer).except(:phones).permit( 
       :email, :password,
       :family_name, :given_name, :family_name_kana, :given_name_kana,
       :birthday, :gender
@@ -58,7 +126,7 @@ class Staff::CustomerForm
   end  
 
   private def home_address_params 
-    @params.require(:home_address).permit(
+    @params.require(:home_address).except(:phones).permit(
       :postal_code, :prefecture, :city, :address1, :address2
     )
   end 
@@ -68,5 +136,16 @@ class Staff::CustomerForm
       :postal_code, :prefecture, :city, :address1, :address2,
       :company_name, :division_name
     )
+  end 
+
+  #private def phone_params(record_name) 
+    #@params.require(record_name)
+      #.slice(:phones).permit(phones: [ :number, :primary ]) 
+  #end
+  
+  private def phone_params(record_name) 
+    @params.require(record_name)
+      .slice(:phones).permit(phones: [ "0", "1" ]
+        .to_h { |key| [ key, [:number, :primary]]}) 
   end 
 end     
---
+++ spec/features/staff/phone_management_spec.rb
@@ -0,0 +1,82 @@
+require "rails_helper" 
+
+feature "職員による顧客電話番号管理" do 
+  include FeaturesSpecHelper 
+  let(:staff_member) { create(:staff_member) }
+  let!(:customer) { create(:customer) } 
+  
+  before do 
+    switch_namespace(:staff)
+    login_as_staff_member(staff_member)
+  end 
+  
+  scenario "職員が顧客の電話番号を追加する" do 
+    click_link "顧客管理"
+
+    #first("table.listing").click_link "編集" 
+    within("table.listing") do 
+      click_link "編集", match: :first
+    end 
+
+    fill_in "form_customer_phones_0_number", with: "090-9999-9999"  
+    #fill_in "form[customer][phones][0][number]", with: "090-9999-9999"
+    #fill_in "電話番号", with: "090-9999-9999"
+    check "form_customer_phones_0_primary"
+    #check "form[customer][phones][0][primary]" 
+    #within(first(".phone-fields")) do # 実際のHTMLのクラス名に合わせてください
+      #check "優先"
+    #end
+    click_button "更新"
+
+    #puts page.text # テスト実行時のターミナルに、その時の画面の全テキストを強制出力します
+    #puts page.html 
+    #save_and_open_page
+    expect(page).to have_content "顧客情報を更新しました。"
+    #expect(page).to have_link "新規登録"
+
+    #customer.reload 
+
+    # データベースのreloadを辞め、画面遷移して登録された電話番号が表示されているか検証する
+    # ※Baukis2の仕様上、更新後は一覧画面（index）に戻るため、対象の顧客の「詳細」リンクをクリックします
+    within("table.listing") do
+      click_link "詳細", match: :first
+    end
+
+    # 【デバッグ用コードを追加】
+    p "=== Customer Errors ==="
+    p customer.errors.full_messages
+    p "=== Phone Errors ==="
+    p customer.personal_phones.map { |p| [p.number, p.errors.full_messages] }
+
+    #expect(customer.personal_phones.size).to eq(1) 
+    #expect(customer.personal_phones[0].number).to eq("090-9999-9999")
+
+     # 詳細画面（show）に、追加した電話番号が表示されていることを確認する
+    expect(page).to have_content "090-9999-9999"
+  end  
+  
+  scenario "職員が顧客の自宅電話番号を追加する" do 
+    click_link "顧客管理"
+
+    #first("table.listing").click_link "編集"
+    within("table.listing") do 
+      click_link "編集", match: :first
+    end  
+
+    fill_in "form_home_address_phones_0_number", with: "03-9999-9999" 
+    check "form_home_address_phones_0_primary" 
+    click_button "更新" 
+
+    expect(page).to have_content "顧客情報を更新しました。" 
+
+    #customer.reload 
+    
+    within("table.listing") do
+      click_link "詳細", match: :first
+    end
+
+    #except(customer.home_address.phones.size).to eq(1)
+    #except(customer.home_address.phones[0].number).to eq("03-9999-9999") 
+    expect(page).to have_content "03-9999-9999"
+  end   
+end     
