c28be65c5e274a2081a99d572fec5210c2e020f1
nakano-shotaro <sononakano@gmail.com>
2026-06-11 12:44:31 +0900

    feat: 検索機能の実装を追加

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -1,10 +1,19 @@
 class Staff::CustomersController < Staff::Base 
   def index 
-    @search_form = Staff::CustomerSearchForm.new 
-    @customers = Customer.order(:family_name_kana, :given_name_kana)
-      .page(params[:page]) 
+    @search_form = Staff::CustomerSearchForm.new(search_params)  
+    #@customers = Customer.order(:family_name_kana, :given_name_kana)
+      #.page(params[:page]) 
+    @customers = @search_form.search.page(params[:page])   
   end 
   
+  private def search_params 
+    params[:search]&.permit([
+      :family_name_kana, :given_name_kana, 
+      :birth_year, :birth_month, :birth_mday, 
+      :address_type, :prefecture, :city, :phone_number 
+    ])
+  end   
+
   def show 
     @customer = Customer.find(params[:id]) 
   end  
