53c74e7d5fc6a8eec405bf8c7b569d86e42aed85
nakano-shotaro <sononakano@gmail.com>
2026-06-23 13:50:35 +0900

    feat: プログラム管理機能(2)を追加

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -5,5 +5,14 @@ class Staff::ProgramsController < Staff::Base
   
   def show 
     @program = Program.listing.find(params[:id]) 
-  end   
+  end  
+  
+  def new 
+    @program = Program.new  
+  end 
+
+  def edit  
+    @program = Program.find(params[:id])
+    @program.init_virtual_attributes   
+  end 
 end
